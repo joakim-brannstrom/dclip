9110915c9e43580dc289733af746ae73ab5e91b1
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2017-06-25 15:37:34 +0200

    implement the basis for main

--- source/app.d
+++ source/app.d
@@ -1,6 +1,33 @@
+/**
+Copyright: Copyright (c) 2017, Joakim Brännström. All rights reserved.
+License: $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost Software License 1.0)
+Author: Joakim Brännström (joakim.brannstrom@gmx.com)
+*/
+module dclip;
+
+import std.path;
 import std.stdio;
 
-void main()
-{
-	writeln("Edit source/app.d to start your project.");
+int pbcopy(string[] args) {
+    return 0;
+}
+
+int pbpaste(string[] args) {
+    return 0;
+}
+
+int main(string[] args) {
+    alias Command = int function(string[] args);
+
+    Command[string] commands;
+    commands["pbcopy"] = &pbcopy;
+    commands["pbpaste"] = &pbpaste;
+
+    if (auto c = args[0].baseName in commands) {
+        return (*c)(args);
+    } else {
+        writeln("Unknown command: ", args[0].baseName);
+    }
+
+    return 1;
 }
